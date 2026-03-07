b4da3c94c8778aef21d1b6b14a4ce058c1033d3b
kis080510 <kis080510@naver.com>
2026-03-08 08:53:48 +0900

    진입 버그 수정

--- lib/start.dart
+++ lib/start.dart
@@ -15,6 +15,7 @@ import 'package:firebase_auth/firebase_auth.dart' hide UserInfo;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gochon_mobile/firebase_options.dart';
 import 'package:gochon_mobile/services/auth_service.dart';
 import 'package:gochon_mobile/services/user_service.dart';
@@ -29,6 +30,7 @@ import 'models/user_info.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await PreferenceManager.initialize();
